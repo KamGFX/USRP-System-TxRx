74f1813630387fcd82b899bed45167550152550d
valepinor <134948393+valepinor@users.noreply.github.com>
2026-06-10 08:52:16 -0500

    Add files via upload

---
+++ cargar2.m
@@ -0,0 +1,10 @@
+clc;
+clear;
+% Leer imagen
+img_tx = imread('C:\Users\juand\Downloads\Copy_of_prueba_final.png');
+% Verificar tamaño
+size(img_tx)
+% Mostrar imagen
+figure;
+imshow(img_tx);
+title('Imagen a transmitir');
